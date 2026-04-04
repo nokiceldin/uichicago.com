88d05523f1583affca2aaa7f944b7cdd751293ff
nokiceldin <nokiceldin2@gmail.com>
2026-04-03 20:30:52 -0400

    fix: exclude prof imports from output tracing

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  outputFileTracingExcludes: {
+    "/*": ["prof_imports/**/*"],
+  },
   async rewrites() {
     return [
       {
